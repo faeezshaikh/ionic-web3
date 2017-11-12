895d3a555fd05eb36cfcd95bc0ee26247be2f0f0
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-11-12 00:57:09 -0600

    Updates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Web3ServiceProvider } from '../../providers/web3-service/web3-service';
+// import { TestProvider } from '../../providers/test/test';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  web3Service: Web3ServiceProvider;
+  smartContract: any;
+  web3: any;
 
-  constructor(public navCtrl: NavController) {
+  blockHash: any;
+  fName: string;
+  lName: string;
+  age: any;
+  showSpinner: boolean = false;
+  constructor(public navCtrl: NavController,public svc: Web3ServiceProvider) {
+    this.web3Service = svc;
+    this.foo();
+  }
 
+  foo () {
+    this.smartContract = this.web3Service.getSmartContract();
+    this.web3 = this.web3Service.get();
+
+
+    var instructorEvent = this.smartContract.instructorInfo({},'latest');
+    console.log('Getting infi...');
+    
+    instructorEvent.watch(function (err, result) {
+            if (!err) {
+                // this.blockHash = result.blockHash;
+                // this.fName = this.web3.toAscii(result.args.fName);
+                // this.lName = this.web3.toAscii(result.args.lName);
+                // this.age = result.args.age;
+                // console.log(result.blockHash);
+                // console.log(this.fName + this.lName);
+                
+                    
+            } else {
+                console.log(err);
+            }
+    });
+  }
+
+  submit() {
+    console.log('submit clicked');
+    
+    this.smartContract = this.web3Service.getSmartContract();
+    this.smartContract.setInstructor(this.web3.eth.defaultAccount,32, "faeez", "shaikh", (err, res) => {
+      if (err) 
+        console.error(err);
+});
   }
 
 }
--- src/providers/web3-service/web3-service.ts
+++ src/providers/web3-service/web3-service.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import Web3 from 'web3';
 
@@ -24,8 +23,36 @@ export class Web3ServiceProvider {
         // Step 3: Get Interface to the contact - ABI from remix
         var CoursetroContract = this.web3.eth.contract([{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"getInstructor","outputs":[{"name":"","type":"uint256"},{"name":"","type":"bytes16"},{"name":"","type":"bytes16"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getInstructors","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"instructorAccts","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"countInstructors","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"},{"name":"_age","type":"uint256"},{"name":"_fName","type":"bytes16"},{"name":"_lName","type":"bytes16"}],"name":"setInstructor","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"fName","type":"bytes16"},{"indexed":false,"name":"lName","type":"bytes16"},{"indexed":false,"name":"age","type":"uint256"}],"name":"instructorInfo","type":"event"}]);
         // Step 4: Get Contract instance at the address from remix
-        this.Coursetro = CoursetroContract.at('0x71d5ea9016f5a0d6f26f098919d7b9fc00dba3d6');
+        this.Coursetro = CoursetroContract.at('0xc0ed63e3a70bfcb003452b1cc083db822e1f23e1');
         console.log("Got the smart contract ==> " , this.Coursetro);
+
+        var instructorEvent = this.Coursetro.instructorInfo({},'latest');
+        console.log('Getting infoo...');
+        
+        let that = this;
+        instructorEvent.watch(function (err, result) {
+                if (!err) {
+                    // this.blockHash = result.blockHash;
+                    // this.fName = this.web3.toAscii(result.args.fName);
+                    // this.lName = this.web3.toAscii(result.args.lName);
+                    // this.age = result.args.age;
+                    console.log(result);
+                    
+                    console.log(result.blockHash);
+                    console.log('Age => ',result.args.age.toString(10));
+                    console.log('Last name: ', that.web3.toAscii(result.args.lName));
+                    console.log('Name: ' , that.web3.toAscii(result.args.fName)) ;
+                    
+                    
+                    
+                    
+                        
+                } else {
+                    console.log(err);
+                }
+              });
+
+    
 
       
   }
@@ -35,7 +62,7 @@ export class Web3ServiceProvider {
     return this.web3;
   }
 
-  getSmartContract() {
+  public getSmartContract() {
     return this.Coursetro;
   }
 
